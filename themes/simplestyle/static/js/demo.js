a8db7017c2bf2edf046d1e9b9d6747b016e53ca6
zuojj <cuew1987@163.com>
2016-09-18 16:11:49 +0800

    0.0.2 release

---
+++ themes/simplestyle/static/js/demo.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @authors Benjamin (zuojj.com@gmail.com)
+ * @date    2016-09-18 15:42:51
+ * @version $Id$
+ */
+
+(function(window, undefined) {
+    var demo = '';
+    function abc() {
+        console.log('test' + demo);
+    }
+
+    abc();
+})(window);
